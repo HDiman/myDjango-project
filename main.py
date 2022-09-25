f46e49cf4a6463a0d258794f5f47928d17d6641b
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-09-25 21:01:32 +0300

    Dataclasses

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 
 
 
-def find_lonely(lst):
-    alone_num = [n for n in lst if n + 1 not in lst and n - 1 not in lst and lst.count(n) < 2]
-    return sorted(alone_num)
+# def find_lonely(lst):
+#     alone_num = [n for n in lst if n + 1 not in lst and n - 1 not in lst and lst.count(n) < 2]
+#     return sorted(alone_num)
+#
+# lst = [16, 30, 5, 5, 5, 3, -1, 0]
+# print(find_lonely(lst))
 
-lst = [16, 30, 5, 5, 5, 3, -1, 0]
-print(find_lonely(lst))
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    name: str
+    age: int
+    profession: str
+
+user1 = User('Alice', 36, 'doctor')
+user2 = User('Bob', 24, 'developer')
+
+print (user1, user2)
